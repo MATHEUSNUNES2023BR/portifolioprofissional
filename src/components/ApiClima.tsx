ce19b631709f72126370641dea3e30243e1de6e3
Matheus Nunes <devmatheusnunesdebarros@gmail.com>
2025-01-16 19:36:44 -0300

    feat: Configurado image loading

--- src/components/ApiClima.tsx
+++ src/components/ApiClima.tsx
@@ -2,6 +2,7 @@ import lupa from '../assets/icons8-magnifying-glass-96.png'
 import localizacao from '../assets/icons8-location-96.png'
 import vento from '../assets/icons8-wind-96.png'
 import umidade from '../assets/icons8-humidity-96.png'
+import loading from '../assets/Spinner@1x-1.0s-200px-200px.gif'
 import { useState } from 'react'
 interface WeatherData {
   coord: {
@@ -56,11 +57,13 @@ function ApiClima(){
   const [wind , setWind] = useState(0)
   const [weather , setWheater] = useState("")
   const [weatherIcon , setWheaterIcon] = useState("")
-  const [stateCard, setStateCard] = useState(false)
+  const [islLoading, setIsLoading] = useState(false)
+  const [activator, setActivator] = useState(false)
   const showWheaterData = async (city: string) =>{
     const apiWheaterURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`
     const res = await fetch(apiWheaterURL)
     const data: WeatherData = await res.json()
+    setIsLoading(true)
     setCityName(data.name)
     setTemperature(Math.trunc(data.main.temp))
     setHumidity(data.main.humidity)
@@ -68,6 +71,7 @@ function ApiClima(){
     setCountryURL(`https://flagsapi.com/${data.sys.country}/flat/32.png`)
     setWheater((data.weather['0'].description))
     setWheaterIcon(data.weather['0'].icon)
+    return 
   }
   return(
     <div className='shadow-md bg-gradient-to-t from-neutral-200 to-sky-400  rounded-md  font-["Poppins"] text-neutral-800 w-[310px] h-auto'>
@@ -75,35 +79,41 @@ function ApiClima(){
         <h3 className='text-xl text-center pt-2'>Confira o clima de uma cidade: </h3>
         <div id="form-input-container" className='flex items-center justify-around w-full mt-5'>
           <input onChange={(e) => setCityState(e.target.value)} className='w-[80%] h-10 border-2 rounded-md border-neutral-700 placeholder-neutral-500 placeholder:p-2' placeholder='Digite o nome da cidade' type="text" name="" id="" />
-          <button onClick={() => {showWheaterData(cityState), setStateCard(!stateCard)}} id="search" className='w-8'>
+          <button onClick={() => {showWheaterData(cityState), setActivator(true), setIsLoading(false)}} id="search" className='w-8'>
             <img className='w-full' src={lupa} alt="" />
           </button>
         </div>
-        <div className={`${stateCard ? '' : 'hidden'} w-full`}>
-          <hr className='text-black w-full mt-2'/>
-          <div className='mt-4 flex-col place-items-center'>
-            <div className='flex items-center justify-evenly w-56 mx-auto mt-7'>
-              <img className='w-6' src={localizacao} alt="Icone Localização" />
-              <span className='text-3xl' id='city'>{cityName}</span>
-              <img className='mt-1' id='country' alt="Bandeira do país" src={countryURL} />
+        {
+        activator ? 
+          (islLoading ? 
+            <div className={`w-full`}>
+              <hr className='text-black w-full mt-2'/>
+              <div className='mt-4 flex-col place-items-center'>
+              <div className='flex items-center justify-evenly w-72 mx-auto mt-7'>
+                <img className='w-6' src={localizacao} alt="Icone Localização" />
+                <span className='text-xl text-center' id='city'>{cityName}</span>
+                <img className='mt-1' id='country' alt="Bandeira do país" src={countryURL} />
+              </div>
+              <p className='text-xl mt-5'><span>{temperature}</span> &deg;C</p>
+              <div className='flex items-center mt-2'>
+                <p className='capitalize'>{weather}</p>
+                <img id='weather-icon' src={`https://openweathermap.org/img/wn/${weatherIcon}.png`} alt="Condições do clima" />
+              </div>
+              <div className='flex justify-around w-48 mt-3'>
+                <p className='flex items-center' id="humidity">
+                  <img className='w-6' src={umidade} alt="Umidade" />
+                  <span className='ms-3'>{humidity}%</span>
+                </p>
+                <p className='flex items-center' id="wind">
+                  <img className='w-6' src={vento} alt="Vento" />
+                  <span className='ms-3'>{wind}km/h</span>
+                </p>
+              </div>
             </div>
-            <p className='text-xl mt-5'><span>{temperature}</span> &deg;C</p>
-            <div className='flex items-center mt-2'>
-              <p className='capitalize'>{weather}</p>
-              <img id='weather-icon' src={`https://openweathermap.org/img/wn/${weatherIcon}.png`} alt="Condições do clima" />
             </div>
-            <div className='flex justify-around w-48 mt-3'>
-              <p className='flex items-center' id="humidity">
-                <img className='w-6' src={umidade} alt="Umidade" />
-                <span className='ms-3'>{humidity}%</span>
-              </p>
-              <p className='flex items-center' id="wind">
-                <img className='w-6' src={vento} alt="Vento" />
-                <span className='ms-3'>{wind}km/h</span>
-              </p>
-            </div>
-          </div>
-        </div>
+            : <img className='w-24' src={loading}/>)
+          : (<div> </div>)
+        }
       </div>
     </div>
   )
